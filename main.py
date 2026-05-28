10e154613031c2293e07894f97b852e020771431
Rebecca Bomfim <rebeccabdantas@gmail.com>
2026-05-28 13:46:26 -0300

    Adicionando persistência com JSON

--- main.py
+++ main.py
@@ -1,15 +1,40 @@
-entregas = []
-def cadastrar_entrtega():
+import json
+# carrega as entregas do arquivo JSON
+def carregar_entregas():
+    # tenta abrir os arquivos
+    try:
+        # abre o arquivo em modo leitura
+        with open("entregas.json", "r", encoding="utf-8") as arquivo:
+            # converte JSON pra lista Python
+            return json.load(arquivo)
+        # caso o arquivo ainda não exista
+    except FileNotFoundError:
+        # retorna lista vazia
+        return []
+    # salva a lista de entregas no arquivo JSON
+def salvar_entregas():
+    # abre o arquivo em modo escrita
+    with open("entregas.json", "w", encoding="utf-8") as arquivo:
+        # converte lista Python para JSON
+        json.dump(entregas, arquivo, indent=4, ensure_ascii=False)
+        
+# lista principal carregada do JSON
+entregas = carregar_entregas()
+def cadastrar_entrega():
     cliente = input("Nome do cliente: ")
     endereco = input("Endereço de entrega: ")
-
+    # estrutura de entrega
     entrega = {
         "cliente": cliente,
         "endereco": endereco,
         "status": "Pendente"
     }
+    # aiciona entrega na lsta
     entregas.append(entrega)
+    # atualiza arquivo JSON
+    salvar_entregas()
     print("Entrega cadastrada com sucesso!")
+# exibe todas as entregas cadastradas
 def listar_entregas():
     if len(entregas) == 0:
         print("Nenhuma entrega cadastrada.")
@@ -25,10 +50,14 @@ def atualizar_status():
             print(f"{indice + 1}. {entrega['cliente']} - {entrega['status']}")
         numero = int(input("Digite o número da entrega: "))
         if 1 <= numero <= len(entregas):
+            # atualiza status da entrega para "Entregue"
             entregas[numero - 1]["status"] = "Entregue"
+            salvar_entregas()
             print("Status atualizado com sucesso!")
         else:
             print("Número inválido.")
+
+# loop principal do sistema
 while True:   
     print("\n=== DELIVERY MANAGER ===")
     print("1 - Cadastrar entrega")
@@ -37,7 +66,7 @@ while True:
     print("4 - Sair")
     opcao = input("Escolha uma opção: ")
     if opcao == "1":
-        cadastrar_entrtega()
+        cadastrar_entrega()
     elif opcao == "2":
         listar_entregas()
     elif opcao == "3":
